b17b848cad68e8de75cd8f4b5da68388e5ee05f2
Thais Kotovicz <thais_kotovicz@hotmail.com>
2024-02-26 21:15:52 -0300

    feat: import typeorm module at app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DATABASE_NAME,
+      entities: [],
+      synchronize: process.env.ENV === 'development',
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })

687ed465eb8d0bdc51e3303e32a13937efb2ab7a
Thais Kotovicz <thais_kotovicz@hotmail.com>
2024-02-27 22:44:24 -0300

    feat: create typeorm data-source

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,9 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { CompanyEntity } from './company/entity/company.entity';
+import { EmployeeEntity } from './employee/entity/employee.entity';
+import { LoanEntity } from './loan/entity/loan.entity';
 
 @Module({
   imports: [
@@ -12,7 +15,7 @@ import { ConfigModule } from '@nestjs/config';
       username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
       database: process.env.DATABASE_NAME,
-      entities: [],
+      entities: [CompanyEntity, EmployeeEntity, LoanEntity],
       synchronize: process.env.ENV === 'development',
     }),
   ],

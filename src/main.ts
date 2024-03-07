43c17236fcb80d9e73c57c053ed1aca4f79f9d6e
Thais Kotovicz <thais_kotovicz@hotmail.com>
2024-03-07 18:55:14 -0300

    chore: set a global prefix

--- src/main.ts
+++ src/main.ts
@@ -7,10 +7,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors();
-
   app.useGlobalInterceptors(new LogInterceptor());
-
   app.useGlobalPipes(new ValidationPipe());
+
+  app.setGlobalPrefix('api/v1.0');
   await app.listen(3000);
 }
 bootstrap();
